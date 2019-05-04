fe3811941619e93e55f62e639901fd40b9e01984
ymir17 <ymir17@ru.is>
2019-05-04 17:27:51 +0000

    push django to repo

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\ymirr\Documents\HR_18-19\Vor_19\VLN2\castleApt\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
