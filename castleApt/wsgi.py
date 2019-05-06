f765aee08853330932b77edadc033c7323aefcdd
ymir17 <ymir17@ru.is>
2019-05-06 12:31:43 +0000

    added more apps and settings

--- castleApt/wsgi.py
+++ castleApt/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for controller project.
+WSGI config for castleApt project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'controller.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'castleApt.settings')
 
 application = get_wsgi_application()
--- home/apps.py
+++ home/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class AdminConfig(AppConfig):
-    name = 'admin'
+class HomeConfig(AppConfig):
+    name = 'home'
