3fc7dd656446e1cd38433d329941ca520fa2b68b
ymir17 <ymir17@ru.is>
2019-05-06 10:30:54 +0000

    added some routing to ./home

--- controller/wsgi.py
+++ controller/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for castleApt project.
+WSGI config for controller project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'castleApt.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'controller.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'castleApt.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'controller.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
