60a75c39d0d97751ff8c3b701f70daa8817e5b8e
ymir17 <ymir17@ru.is>
2019-05-06 16:26:08 +0000

    added table classes to home, contacts and signup

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from signup.models import Accounts
 
-# Create your models here.
+
+class RealtorImages(models.Model):
+    realtImgId = models.AutoField(primary_key=True)
+    realtImgUrl = models.CharField(1024)
+
+
+class Realtors(models.Model):
+    realtId = models.AutoField(primary_key=True)
+    accountId = models.ForeignKey(Accounts, on_delete=models.CASCADE())
+    description = models.CharField(max_length=1024)
+    realtImgId = models.ForeignKey(RealtorImages, on_delete=models.CASCADE())
--- home/models.py
+++ home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from contacts.models import Realtors
 
-# Create your models here.
+
+class PropImages(models.Model):
+    imgId = models.AutoField(primary_key=True)
+    propImgUrl = models.CharField(max_length=1024)
+
+
+class Properties(models.Model):
+    address = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    zipCode = models.IntegerField()
+    rooms = models.IntegerField()
+    size = models.FloatField()
+    price = models.FloatField()
+    imgId = models.ForeignKey(PropImages, on_delete=models.CASCADE())
+    realtId = models.ForeignKey(Realtors, on_delete=models.CASCADE())
