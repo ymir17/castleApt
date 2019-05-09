b2af1f2f0a5a256ef9f0ebb7f419f0a78f267fb1
ymir17 <ymir17@ru.is>
2019-05-09 19:24:40 +0000

    gitignore changes and added some sql scripts

---
+++ populate_scripts/generate_properties_img.py
@@ -0,0 +1,36 @@
+# id,propImgUrl, porpertyId_id
+
+LINE = 'INSERT INTO property_propimages ("propImgUrl", "propertyId_id")\nVALUES ('
+PATH = 'static/'
+
+def populateCastle(f):
+    for i in range(5):
+        for j in range(4):
+            f.write(LINE \
+                + "'" + PATH + 'castles/0' + str(i) + '_0' + str(j) + '.jpeg' + "', " \
+                + str(i+1) + ');\n')
+
+def populateVilla(f):
+    for i in range(5):
+        for j in range(4):
+            f.write(LINE \
+                + "'" + PATH + 'villas/0' + str(i) + '_0' + str(j) + '.jpeg' + "', " \
+                + str(i+6) + ');\n')
+
+def populateCabin(f):
+    for i in range(5):
+        for j in range(2):
+            f.write(LINE \
+                + "'" + PATH + 'cabins/0' + str(i) + '_0' + str(j) + '.jpeg' + "', " \
+                + str(i+11) + ');\n')
+
+def main():
+    f = open('propertiesImg.sql', 'w')
+    
+    populateCastle(f)
+    populateVilla(f)
+    populateCabin(f)
+    
+    f.close()
+
+main()
