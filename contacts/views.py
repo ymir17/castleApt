126715233096833e153d6188227426fabea8943e
ymir17 <ymir17@ru.is>
2019-05-13 22:35:18 +0000

    contacts.html fixed

--- contacts/views.py
+++ contacts/views.py
@@ -10,18 +10,3 @@ def index(request):
     realtor = Realtors.objects.all()
     context = {'accounts': account, 'realtorImgs': realtorImg, 'realtors': realtor}
     return render(request, 'Contacts/contacts.html', context)
-
-
-
-# def getRealtors(request, id):
-#     if id == Realtors.objects.get(accountId=id).accountId_id:
-#         realtors = {
-#             'name': str(Accounts.objects.get(accountId=id).firstName) + ' ' + str(Accounts.objects.get(accountId=id).lastName),
-#             'description': str(Realtors.objects.get(accountId=id).description),
-#             'phoneNo': str(Accounts.objects.get(accountId=id).phoneNo),
-#             'email': str(Accounts.objects.get(accountId=id).email),
-#             'image': RealtorImages.objects.get(realtImgId=(Realtors.objects.get(accountId=id).realtImgId_id)).realtImgUrl
-#         }
-#
-#         return render(request, realtors)
-#     raise Http404('Realtor does not exists')
