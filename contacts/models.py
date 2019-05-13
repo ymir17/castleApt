c2723f6722ce1896dd731b36e9e3ba313c40f313
ymir17 <ymir17@ru.is>
2019-05-13 15:03:38 +0000

    contacts fixed

--- contacts/models.py
+++ contacts/models.py
@@ -5,13 +5,13 @@ from signup.models import Accounts
 class RealtorImages(models.Model):
     realtImgId = models.AutoField(auto_created=True, primary_key=True)
     realtImgUrl = models.CharField(max_length=1024)
-    def __str__(self):
-        return self.realtImgUrl
 
+    def getName(self, *args):
+            return Accounts.objects.get(accountId=args).firstName
 
 
 class Realtors(models.Model):
-    #name = models.CharField(max_length=255)
+    # name = models.CharField(max_length=255)
     realtId = models.AutoField(auto_created=True, primary_key=True)
     accountId = models.ForeignKey(Accounts, on_delete=models.CASCADE)
     description = models.CharField(max_length=1024)
--- contacts/views.py
+++ contacts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import Http404
 from contacts.models import Realtors, RealtorImages
 from signup.models import Accounts
 
@@ -9,3 +10,18 @@ def index(request):
     realtor = Realtors.objects.all()
     context = {'accounts': account, 'realtorImgs': realtorImg, 'realtors': realtor}
     return render(request, 'Contacts/contacts.html', context)
+
+
+
+# def getRealtors(request, id):
+#     if id == Realtors.objects.get(accountId=id).accountId_id:
+#         realtors = {
+#             'name': str(Accounts.objects.get(accountId=id).firstName) + ' ' + str(Accounts.objects.get(accountId=id).lastName),
+#             'description': str(Realtors.objects.get(accountId=id).description),
+#             'phoneNo': str(Accounts.objects.get(accountId=id).phoneNo),
+#             'email': str(Accounts.objects.get(accountId=id).email),
+#             'image': RealtorImages.objects.get(realtImgId=(Realtors.objects.get(accountId=id).realtImgId_id)).realtImgUrl
+#         }
+#
+#         return render(request, realtors)
+#     raise Http404('Realtor does not exists')
