8400f0f0a77b2309642881e14714e18e4d5f0ba5
ymir17 <ymir17@ru.is>
2019-05-14 16:55:49 +0000

    db modification

--- signup/urls.py
+++ signup/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     # path('', views.index, name='signup-index'),
     # path('<int:id>', views.getUser, name='getUser-index'),
-    path('', views.createUser, name='createUser'),
     path('', views.index, name='signup-index'),
-    #path('create_user', views.createUser, name='createUser'),
+    # path('create_user', views.createUser, name='createUser'),
 ]
