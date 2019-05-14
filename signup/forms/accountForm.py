0d951cede35cee4c267b7914153306f3af4a91bc
ymir17 <ymir17@ru.is>
2019-05-14 16:28:06 +0000

    cleared conflicts

--- signup/forms/accountForm.py
+++ signup/forms/accountForm.py
@@ -1,11 +1,5 @@
-from django.core.exceptions import ValidationError
-<<<<<<< HEAD
 from django import forms
-from django.forms import ModelForm, widgets
-=======
 from django.forms import ModelForm, forms, widgets
-from django import forms
->>>>>>> 86b65efda2a86a9f80ea2bf9be255fca9cbbbbb4
 from signup.models import Accounts, Users
 
 
@@ -20,26 +14,16 @@ class CreateAccountForm(ModelForm):
             'phoneNo': widgets.NumberInput(attrs={'class': 'inputs', 'placeholder': 'Phone Number'}),
             'email': widgets.EmailInput(attrs={'class': 'inputs', 'placeholder': 'Email'}),
             'password': widgets.PasswordInput(attrs={'class': 'inputs', 'placeholder': 'Password'}),
-<<<<<<< HEAD
             # 're-enter_pw': widgets.PasswordInput(attrs={'class': 'inputs', 'placeholder': 'Re-enter Password'}),
         }
     # pw_validation = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'class': 'inputs',
     #                                                                               'placeholder': 'Re-enter Password'}))
-    #
-    # def is_pw_valid(self):
-    #     pw1 = self.Meta.widgets['password']
-    #     return pw1 == self.pw_validation
-=======
-            #'re-enter_pw': widgets.PasswordInput(attrs={'class': 'inputs', 'placeholder': 'Re-enter Password'}),
-        }
-    #pw_validation = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'class': 'inputs',
-                                                                                #'placeholder': 'Re-enter Password'}))
+
     def is_pw_valid(self):
         pw1 = self.Meta.widgets['password']
-        #pw2 = self.Meta.widgets['re-enter_pw']
+        # pw2 = self.Meta.widgets['re-enter_pw']
         pw2 = self.pw_validation
         return pw1 == pw2
->>>>>>> 86b65efda2a86a9f80ea2bf9be255fca9cbbbbb4
 
 
 class CreateUserForm(ModelForm):
--- signup/views.py
+++ signup/views.py
@@ -7,59 +7,29 @@ from signup.models import Accounts, Users
 
 
 def index(request):
-<<<<<<< HEAD
-    return render(request, 'Sign_up/signup.html')
-
-
-def createUser(request):
-    # TODO: Try to implement single form to two separate models!
-    if request.method == 'POST':
-        newAccount = CreateAccountForm(data=request.POST)
-        newUser = CreateUserForm(data=request.POST)
-        form = inlineformset_factory(Accounts, Users, fields=('accountId',))
-        # if newAccount.is_pw_valid():
-        if newAccount.is_valid() and newUser.is_valid():
-            account = newAccount.save()
-            user = newUser.save()
-            sum = form.save()
-            return redirect('home-index')
-=======
-    # def createUser(request):
     # TODO: Try to implement single form to two separate models!
     if request.method == 'POST':
         print(request.method)
         form = CreateAccountForm(data=request.POST)
-        newUsert = CreateUserForm(data=request.POST)
+        newUser = CreateUserForm(data=request.POST)
         #if form.is_pw_valid():
         if form.is_valid():
             print("VALID ACCOUNT")
             # form = inlineformset_factory(Accounts, Users, fields=('accountId',))
             form.save()
-            if newUsert.is_valid():
+            if newUser.is_valid():
                 print("VALID USER")
-                newUsert.save()
+                newUser.save()
             # account = accountForm.save()
                 return redirect('home-index')
->>>>>>> 86b65efda2a86a9f80ea2bf9be255fca9cbbbbb4
         else:
             print("NOT VALID")
             pass
     else:
-<<<<<<< HEAD
-        form = CreateAccountForm()
-    return render(request, 'Sign_up/signup.html', {
-        'form': form
-=======
-        print(request.method)
-        newUsert = CreateUserForm()
         form = CreateAccountForm()
     return render(request, 'Sign_up/signup.html', {
 
-        'form': form, 'form2': newUsert
->>>>>>> 86b65efda2a86a9f80ea2bf9be255fca9cbbbbb4
+        'form': form, 'form2': newUser
     })
-def getUser(request):
-    pass
-
 
 
