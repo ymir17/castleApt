b79d98b505b103a5abb169a5a07d236e749e5b6e
ymir17 <ymir17@ru.is>
2019-05-15 18:45:04 +0000

    added global variables to home/views

--- home/views.py
+++ home/views.py
@@ -1,6 +1,75 @@
 from django.shortcuts import render
 
+ZIP_CODES = (
+    '109 Reykjavik',
+    '112 Reykjavik',
+    '220 Hafnafjörður',
+    '311 Borgarnes',
+    '531 Hvammstangi',
+    '670 Kópasker',
+    '740 Neskaupstaður',
+    '870 Vík',
+    '109 Reykjavik',
+    '112 Reykjavik',
+    '220 Hafnafjörður',
+    '311 Borgarnes',
+    '531 Hvammstangi',
+    '670 Kópasker',
+    '740 Neskaupstaður',
+    '870 Vík',
+)
+PRICES = (
+    '1',
+    '2',
+    '5',
+    '10',
+    '15',
+    '20',
+    '30',
+    '40',
+    '50',
+)
+SIZES = (
+    '40',
+    '50',
+    '60',
+    '70',
+    '80',
+    '90',
+    '100',
+    '150',
+    '200',
+    '300',
+    '400',
+    '600',
+    '800',
+    '1000',
+)
+ROOMS = (
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '10',
+    '20',
+    '30',
+    '40',
+    '50',
+    '75',
+    '100',
+    '150',
+)
+TYPES = (
+    'Castle',
+    'Villa',
+    'Cabin',
+)
 
-# Create your views here.
+
+def searchForm():
+    pass
+
+
 def index(request):
     return render(request, 'Home/home.html')
