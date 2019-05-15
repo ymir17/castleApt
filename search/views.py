969baecec55745af00162623b7c65141a3580437
Arnar Ólafsson <arnaro17@ru.is>
2019-05-15 17:34:35 +0000

    changed country in payment to be a select dropdown

--- search/views.py
+++ search/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
-from
+
 
 # Create your views here.
 
 def index(request):
-    properties = {'property': }
+
     return render(request, 'search/search.html')
