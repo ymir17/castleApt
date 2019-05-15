5c4692d6e4185329e81b8efe6c5ff2cd0242e1c9
Arnar Ólafsson <arnaro17@ru.is>
2019-05-15 02:04:49 +0000

    merge

--- search/views.py
+++ search/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from
 
 # Create your views here.
 
 def index(request):
+    properties = {'property': }
     return render(request, 'search/search.html')
