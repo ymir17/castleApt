7752d9d63ab4466a14a9e7ff7d38ab3e9dbdbfa4
ymir17 <ymir17@ru.is>
2019-05-15 23:47:41 +0000

    updated search field in home to modelform

--- search/forms/forms.py
+++ search/forms/forms.py
@@ -20,33 +20,36 @@ ZIP_CODES = [
     ('870', '870 Vík'),
 ]
 PRICES = [
-    ('1', '1'),
-    ('2', '2'),
-    ('5', '5'),
-    ('10', '10'),
-    ('15', '15'),
-    ('20', '20'),
-    ('30', '30'),
-    ('40', '40'),
-    ('50', '50'),
+    ('#', ''),
+    ('1', '€1m'),
+    ('2', '€2m'),
+    ('5', '€5m'),
+    ('10', '€10m'),
+    ('15', '€15m'),
+    ('20', '€20m'),
+    ('30', '€30m'),
+    ('40', '€40m'),
+    ('50', '€50m'),
 ]
 SIZES = [
-    ('40', '40'),
-    ('50', '50'),
-    ('60', '60'),
-    ('70', '70'),
-    ('80', '80'),
-    ('90', '90'),
-    ('100', '100'),
-    ('150', '150'),
-    ('200', '200'),
-    ('300', '300'),
-    ('400', '400'),
-    ('600', '600'),
-    ('800', '800'),
-    ('1000', '1000'),
+    ('#', ''),
+    ('40', '40 sq. ft'),
+    ('50', '50 sq. ft'),
+    ('60', '60 sq. ft'),
+    ('70', '70 sq. ft'),
+    ('80', '80 sq. ft'),
+    ('90', '90 sq. ft'),
+    ('100', '100 sq. ft'),
+    ('150', '150 sq. ft'),
+    ('200', '200 sq. ft'),
+    ('300', '300 sq. ft'),
+    ('400', '400 sq. ft'),
+    ('600', '600 sq. ft'),
+    ('800', '800 sq. ft'),
+    ('1000', '1000 sq. ft'),
 ]
 ROOMS = [
+    ('#', ''),
     ('1', '1'),
     ('2', '2'),
     ('3', '3'),
@@ -75,12 +78,14 @@ class searchForm(forms.Form):
     )
     checkboxZip = forms.MultipleChoiceField(
         required=False,
-        widget=forms.CheckboxInput,
+        widget=forms.CheckboxSelectMultiple,
         choices=ZIP_CODES,
+        label= 'Zip Codes',
     )
     pricesLow = forms.CharField(
         required=False,
         widget=forms.Select(choices=PRICES,),
+
     )
     pricesHigh = forms.CharField(
         required=False,
@@ -120,22 +125,22 @@ class searchForm(forms.Form):
             'class': 'container',
         })
         self.fields['pricesLow'].widget.attrs.update({
-            'class': 'container',
+            'class': 'alldropdowns',
         })
         self.fields['pricesHigh'].widget.attrs.update({
-            'class': 'container',
+            'class': 'alldropdowns',
         })
         self.fields['sizesLow'].widget.attrs.update({
-            'class': 'container',
+            'class': 'alldropdowns',
         })
         self.fields['sizesHigh'].widget.attrs.update({
-            'class': 'container',
+            'class': 'alldropdowns',
         })
         self.fields['roomsLow'].widget.attrs.update({
-            'class': 'container',
+            'class': 'alldropdowns',
         })
         self.fields['roomsHigh'].widget.attrs.update({
-            'class': 'container',
+            'class': 'alldropdowns',
         })
         self.fields['types'].widget.attrs.update({
             'class': 'container',
