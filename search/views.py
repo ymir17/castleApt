33bf3b2977dc4246e66dabb3be2c95d51f565688
Arnar Ólafsson <arnaro17@ru.is>
2019-05-16 00:50:30 +0000

    search.html starting to look good

--- search/views.py
+++ search/views.py
@@ -10,6 +10,6 @@ SORT_BY = (
 
 def index(request):
     properties = Properties.objects.all()
-    propimgs = PropImages.objects.all().order_by("propertyId_id")
+    propimgs = PropImages.objects.filter(propImgUrl__contains='_00').order_by("propertyId_id")
     context = {'properties': properties, 'propimgs': propimgs}
     return render(request, 'search/search.html', context)
