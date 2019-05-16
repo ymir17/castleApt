5231f7fc5e1d2656b1170fafc92b0ad5483d6fc6
Arnar Ólafsson <arnaro17@ru.is>
2019-05-16 01:51:59 +0000

    trying something with property

--- property/urls.py
+++ property/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.index, name='property-index'),
-    path('<int:id', get_prop_by_id, name='prop-details')
+    path('<int:id>', views.index, name='property-index'),
+
 ]
