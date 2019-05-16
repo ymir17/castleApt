334b4716e5074a421b9131d0c2c1c85503142bcb
Arnar Ólafsson <arnaro17@ru.is>
2019-05-16 15:54:08 +0000

    property.html now shows the realtor

--- search/views.py
+++ search/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from property.models import Properties, PropImages
+from contacts.models import Realtors
+from signup.models import Accounts
 
 SORT_BY = (
     'Price Low',
@@ -18,9 +20,13 @@ def get_prop_by_id(request, id):
     propimgs = PropImages.objects.all().order_by('propImgUrl')
     frontimgs = PropImages.objects.filter(propImgUrl__contains='_00').order_by("propertyId_id")
     props = Properties.objects.all()
+    realtors = Realtors.objects.all()
+    accounts = Accounts.objects.all()
     return render(request, 'Property/property.html', {
         'property': get_object_or_404(Properties, pk=id),
         'images': propimgs,
         'allprops': props,
-        'frontimg': frontimgs
+        'frontimg': frontimgs,
+        'realtor': realtors,
+        'account': accounts
     })
