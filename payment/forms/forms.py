85f92ae36cad52589e3bb291f186d68de774021f
Arnar Ólafsson <arnaro17@ru.is>
2019-05-16 17:32:58 +0000

    payment.html now has a form

--- payment/forms/forms.py
+++ payment/forms/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.forms import ModelForm
+from django_countries import countries
+from django_countries.fields import CountryField
 
 CARD_TYPE = [
     ('Visa', 'visa'),
@@ -8,4 +10,103 @@ CARD_TYPE = [
 ]
 
 class PaymentForm(forms.Form):
-    cardTypes = forms.Multi
+    cardTypes = forms.ChoiceField(
+        required=True,
+        widget=forms.RadioSelect,
+        choices=CARD_TYPE,
+    )
+    nameOnCard = forms.CharField(
+        required=True,
+        widget=forms.TextInput,
+    )
+    cardNumber = forms.IntegerField(
+        required=True,
+        widget=forms.TextInput,
+    )
+    expDate = forms.CharField(
+        required=True,
+        widget=forms.DateInput,
+    )
+    cvc = forms.IntegerField(
+        required=True,
+        widget=forms.TextInput,
+    )
+    bAddress = forms.CharField(
+        required=True,
+        widget=forms.TextInput,
+    )
+    houseNumber = forms.IntegerField(
+        required=True,
+        widget=forms.TextInput,
+    )
+    city = forms.CharField(
+        required=True,
+        widget=forms.TextInput,
+    )
+    country = forms.ChoiceField(
+        required=True,
+        choices=list(countries),
+    )
+    zipCode = forms.IntegerField(
+        required=True,
+        widget=forms.TextInput,
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['cardTypes'].widget.attrs.update({
+            'type': 'radio',
+            'name': 'card',
+        })
+        self.fields['nameOnCard'].widget.attrs.update({
+            'class': 'inputs',
+            'placeholder': 'Name on card',
+            'name': 'cardname',
+            'type': 'text',
+        })
+        self.fields['cardNumber'].widget.attrs.update({
+            'class': 'inputs',
+            'placeholder': 'Card number',
+            'name': 'cardnumber',
+            'type': 'number',
+        })
+        self.fields['expDate'].widget.attrs.update({
+            'class': 'inputs',
+            'placeholder': 'Exp. date',
+            'name': 'Exp-date',
+            'type': 'date',
+        })
+        self.fields['cvc'].widget.attrs.update({
+            'class': 'inputs',
+            'placeholder': 'CVC',
+            'name': 'CVC',
+            'type': 'number',
+        })
+        self.fields['bAddress'].widget.attrs.update({
+            'class': 'inputs',
+            'placeholder': 'Billing address',
+            'name': 'address',
+            'type': 'text',
+        })
+        self.fields['houseNumber'].widget.attrs.update({
+            'class': 'inputs',
+            'placeholder': 'House number',
+            'name': 'housenumber',
+            'type': 'number',
+        })
+        self.fields['city'].widget.attrs.update({
+            'class': 'inputs',
+            'placeholder': 'City',
+            'name': 'city',
+            'type': 'text',
+        })
+        self.fields['country'].widget.attrs.update({
+            'class': 'inputs',
+        })
+        self.fields['zipCode'].widget.attrs.update({
+            'class': 'inputs',
+            'placeholder': 'Postal code',
+            'name': 'postalcode',
+            'type': 'number',
+        })
+
--- payment/views.py
+++ payment/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from property.models import Properties, PropImages
+from payment.forms.forms import PaymentForm
 
 # Create your views here.
 
 def index(request, id):
     return render(request, 'Payment/payment.html', {
+        'form': PaymentForm(data=request.GET),
         'property': get_object_or_404(Properties, pk=id),
         'images': PropImages.objects.filter(propImgUrl__contains='_00').order_by("propertyId_id")
     })
