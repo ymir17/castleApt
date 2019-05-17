6a11dcd2ad403d0de8394d51f01a66f86951e489
ymir17 <ymir17@ru.is>
2019-05-17 04:39:55 +0000

    removed conflicts

--- search/views.py
+++ search/views.py
@@ -105,10 +105,6 @@ def index(request):
         )
 
         propimgs = PropImages.objects.filter(propImgUrl__contains='_00').order_by("propertyId_id")
-
-        context = {'properties': properties, 'propimgs': propimgs, 'form': orderByForm(data=request.GET)}
-
-        context = {'properties': properties, 'propimgs': propimgs, 'form': orderBy(request.GET)}
         context = {'properties': properties, 'propimgs': propimgs,}
         return render(request, 'search/search.html', context)
     return redirect('home-index')
