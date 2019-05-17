6a537b949ab73466f510b4a811dad443e81fe64f
ymir17 <ymir17@ru.is>
2019-05-17 03:10:40 +0000

    iforget

--- search/views.py
+++ search/views.py
@@ -105,7 +105,7 @@ def index(request):
         )
 
         propimgs = PropImages.objects.filter(propImgUrl__contains='_00').order_by("propertyId_id")
-        context = {'properties': properties, 'propimgs': propimgs, 'form': orderBy(request.GET)}
+        context = {'properties': properties, 'propimgs': propimgs,}
         return render(request, 'search/search.html', context)
     return redirect('home-index')
 
@@ -124,7 +124,3 @@ def get_prop_by_id(request, id):
         'realtor': realtors,
         'account': accounts
     })
-
-
-def orderBy(request):
-    print(request)
