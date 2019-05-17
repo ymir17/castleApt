23ef7443c67f5a7e4775b6a56eae9f08918a12da
ymir17 <ymir17@ru.is>
2019-05-17 15:17:18 +0000

    implemented autofill in payments

--- paymentreview/views.py
+++ paymentreview/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    print(request.POST)
+    # print(request.user.id)
     return render(request, 'Paymentreview/paymentreview.html')
